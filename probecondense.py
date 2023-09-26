0d001c8e829f29766c17114c7428b0971fe7e2a6
RPINerd <RPINerd@gmail.com>
2023-09-26 15:33:32 -0400

    Added arguments and basic parsing

--- probecondense.py
+++ probecondense.py
@@ -6,17 +6,28 @@
 """
 
 import argparse
-import os
-import sys
+
+import probemap
 
 
-def parse_args() -> argparse.ArgumentParser:
+def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--probes", help="Path to the probes file", required=True)
+    parser.add_argument("-t", "--targets", help="Path to the targets file", required=True)
     args = parser.parse_args()
     return args
 
 
 def main(args) -> None:
+    probes = []
+    targets = []
+    with open(args.probes, "r") as probesFile:
+        probes.append(probesFile.readlines())
+    print(probes)
+    with open(args.targets, "r") as targetsFile:
+        targets.append(targetsFile.readlines())
+    print(targets)
+    probemap.main(probes, targets)
     return
 
 
